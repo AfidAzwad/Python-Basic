a31913615c062fe4b109b58422c624ecf6a93a42
Azwad22 <49253433+Azwad22@users.noreply.github.com>
2020-07-23 13:14:09 +0600

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,10 @@
+tup = (2,4,6,7,8,9,11,22)
+
+print(tup)
+print(tup.count(6)) #counting occurence of 6
+
+print(tup.index(2)) #finding the value of index 2
+print(tup.__len__()) #finding the length of tup
+
+tup2 = (99,44,55,88)
+print(tup.__add__(tup2)) #adding 2 tuples
