21a82b7e238eb29db39f7437756f6fb9eb8fb46f
Azwad22 <mdazwad22@gmail.com>
2021-11-09 12:08:47 +0600

    type checking

---
+++ Type checking/typechecking(Array & List).py
@@ -0,0 +1,18 @@
+from array import array
+import numpy as np
+
+
+Lists = [5,4,3,2,21, "ami"]
+
+arr =  np.array([6,8,9,9,0,"ami"])
+
+arry = array('i',[1,2,3]) # i = integer
+
+Arry = array('f',[1.9,2.3,3]) # f = float values
+
+
+print(type(Lists))
+print(type(arr))
+print(type(arry))
+print(type(Arry))
+
