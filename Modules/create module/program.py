d815e2f5c1a29c195b9940690a5871daaa389e3a
Azwad22 <mdazwad22@gmail.com>
2021-11-19 00:17:06 +0600

    module create

---
+++ Modules/create module/program.py
@@ -0,0 +1,7 @@
+import trial
+
+print(trial.__name__)
+
+#Output:
+# My name is  trial =>cause trial considering as module
+# trial => printing module name
---
+++ Modules/create module/trial.py
@@ -0,0 +1,6 @@
+print("My name is ", __name__)
+
+
+#Output:
+# My name is  __main__ =>cause trial is not considering as module here
+# so " if __name__ == '__main__': "
