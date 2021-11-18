6ef474dfc3181fa7b35e716bb9a35538c337846c
Azwad22 <mdazwad22@gmail.com>
2021-11-18 21:48:52 +0600

    datetime

---
+++ Date Time/datetimeModule.py
@@ -0,0 +1,12 @@
+import datetime
+
+print(type(datetime))
+
+today = datetime.date.today()
+print(today)
+
+today = datetime.datetime.today()
+print(today)
+
+today = datetime.datetime.now()
+print(today)
