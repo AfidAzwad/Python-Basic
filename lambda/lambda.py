18a411dbffb7de3c5db7bc648ffad03aba1cf510
Azwad22 <mdazwad22@gmail.com>
2021-11-18 22:27:09 +0600

    updates

--- lambda/lambda.py
+++ lambda/lambda.py
@@ -1,19 +1,20 @@
-#lambda function is a small anonymous function.
-#It can take any number of arguments, but can only have one expression
+# lambda function is a small anonymous function.
+# It can take any number of arguments, but can only have one expression
 
 #syntax = lambda arguments : expression
 
-x = lambda a, b, c, d, e : a + b - c * d / e
+def x(a, b, c, d, e): return a + b - c * d / e
 
-print(x(5, 6, 2, 0, 2)) #5+6-2*0/2
-print(int(x(5, 6, 2, 0, 2)))
+
+print(x(5, 6, 2, 0, 2))  # 5+6-2*0/2
 
 
 def func(n):
-  return lambda a : a * n
+    return lambda a: a * n
+
 
 x = func(2)
 y = func(3)
 
-print(x(11))
-print(y(11))
+print(x)
+print(y)
