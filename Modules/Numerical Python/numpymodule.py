431ecd654182d55a573c400b26a6f9e2532bbbe4
Azwad22 <mdazwad22@gmail.com>
2021-11-19 23:19:55 +0600

    numpy

---
+++ Modules/Numerical Python/numpymodule.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+print(np.divide(8,2))
+
+a = np.array ( [1,2,3] )
+print(a)
+print(type(a))
+
+c = np.array([ [(1,3), (4,6)], [(3,1), (4,6)] ], dtype= int)
+print(c)
+print(type(c))
+
+print("\nexponential : ", np.exp(c))
+
+print("\nsin : ", np.sin(c))
